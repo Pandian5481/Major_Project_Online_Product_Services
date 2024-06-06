ce189ce6876d22b157f1be2ec0bdc3cc0e7eeea5
Pandian5481 <pndn.dnl@gmail.com>
2024-06-06 23:49:43 +0530

    Update productActions.js

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -7,7 +7,7 @@ const URL='http://localhost:8000';
 
 export const getProducts=()=>async(dispatch)=>{
     try {
-        const { data }=await axios.get(`${URL}/products`);
+        const { data }=await axios.get(`https://auzm.vercel.app/products`);
         dispatch({type: actionTypes.GET_PRODUCTS_SUCCESS, payload:data});
     } catch (error) {
         dispatch({type: actionTypes.GET_PRODUCTS_FAIL, payload:error.message});
@@ -18,7 +18,7 @@ export const getProductDetails=(id)=>async(dispatch)=>{
     try {
         dispatch({type: actionTypes.GET_PRODUCT_DETAILS_REQUEST});
 
-        const { data }=await axios.get(`${URL}/product/${id}`);
+        const { data }=await axios.get(`https://auzm.vercel.app/product/${id}`);
 
         dispatch({type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS, payload:data});
     } catch (error) {

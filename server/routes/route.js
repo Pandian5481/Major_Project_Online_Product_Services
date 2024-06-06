b1a5f72212f254d33c6cb061dd73b31e945be35e
Pandian5481 <pndn.dnl@gmail.com>
2024-06-07 00:31:47 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { userSingup, userLogin } from '../controller/user-controller.js';
 import { getProducts, getProductById } from '../controller/product-controller.js';
-import { addPaymentGateway, paytmResponse } from '../controller/payment-controller.js';
+//import { addPaymentGateway, paytmResponse } from '../controller/payment-controller.js';
 import { userShop } from '../controller/usershop-controller.js';
 import { getMyShops, getPendingShops } from '../controller/usershop-controller.js';
 import { getOrderedProducts, postOrderedProducts } from '../controller/orderedproduct-controller.js';

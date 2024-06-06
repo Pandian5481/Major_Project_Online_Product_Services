68ba9644beafc9c5d24a505980beba5fe38e44f4
Pandian5481 <pndn.dnl@gmail.com>
2024-06-06 19:11:17 +0530

    Update db.js
    changed

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const Connection=async(username,password)=>{
     try {
-        const URL='mongodb://127.0.0.1:27017/ECOMMERCE';
+        const URL='mongodb+srv://insomniamusic999:<password>@cluster0.sqtldip.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
         await mongoose.connect(URL,{ useUnifiedTopology:true, useNewUrlParser:true });
         console.log("Database connected");
     } catch (error) {
